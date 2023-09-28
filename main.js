1465054ffaf6596aa09ef8ba5020dccdb6cba471
Peter <85614837+underSkord@users.noreply.github.com>
2023-09-28 13:18:58 +0200

    basic functionality

--- main.js
+++ main.js
@@ -6,35 +6,64 @@
 // name
 // the location of the country (for example hungary, and we display that it is in europe)
 
+// we are using https://restcountries.com , thanks for the free api
+
 const form = document.querySelector("form");
 const searchInput = document.querySelector("#search");
 const searchButton = document.querySelector("#search__button");
 const displayedCountry = document.querySelector("#displayed__country");
+const countryFlag = document.querySelector("#flag");
+const countryName = document.querySelector("#country__name");
+const countryLocation = document.querySelector("#country__location");
+const countryInfo = document.querySelector("#country__info");
 
 window.addEventListener("load", () => {
-  searchInput.value = ""; // first we should set the input field to nothing
+  searchInput.value = "";
 });
+const countries = [];
 
 async function requestCountryByName(inputValue) {
   try {
-    const res = await fetch(
-      `https://restcountries.com/v3.1/name/${inputValue}`
-    ); // we want to use literals to dynamically insert stuff.
-    const country = await res.json();
-    console.log(country);
+    const res = await fetch(`https://restcountries.com/v3.1/name/${inputValue}`);
+    const results = await res.json();
+    countries.splice(0, countries.length);
+    const upperCaseSearch = searchInput.value.charAt(0).toUpperCase() + searchInput.value.slice(1);
+    console.log(upperCaseSearch);
+    countries.push(...results);
+    for (const country of results) {
+      if (country.name.common === upperCaseSearch) {
+        console.log(country); // now its working how we want it to. we search iran, lowercase, it gets turned into uppercase, and since we check the common name it won't insert stuff like Kūki Āirani
+        countryFlag.src = country.flags.png;
+        countryName.firstChild.textContent = country.name.common;
+        countryLocation.firstChild.textContent = country.region;
+        countryInfo.firstChild.textContent = country.capital;
+      }
+    }
   } catch (error) {
     console.log(error);
   }
+  searchInput.value = "";
 }
+
+//currently if the input value is done right. ie. we want to search for italy and actually write italy, it works. also the search is not case sensitive so we can have italy, Italy or even ItaLy it will still work.
+// we are also trimming just in case so "   italy   " becomes "italy" and it still works.
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
 });
 
 searchButton.addEventListener("click", () => {
-  let inputValue = searchInput.value;
+  let inputValue = searchInput.value.trim();
   if (inputValue === "" || inputValue === null) {
     alert("You have to populate the search bar, before you can search");
   }
-  requestCountryByName(inputValue);
+  if (inputValue.includes(" ")) {
+    inputValue = inputValue.replace(" ", "%20");
+  }
+  if (isNaN(searchInput.value)) {
+    requestCountryByName(inputValue);
+  } else {
+    alert("You can't search for a number");
+    searchInput.value = "";
+  }
 });
